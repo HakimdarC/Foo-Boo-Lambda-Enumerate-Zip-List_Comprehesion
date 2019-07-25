98c3aecf154fa901e4110c376bfe11d59d21c30f
HakimdarC <45515325+HakimdarC@users.noreply.github.com>
2019-07-25 18:43:25 +0100

    Add files via upload
    first commit

---
+++ fooprac.py
@@ -0,0 +1,48 @@
+def boo(*arg):
+    return arg
+def foo(**kwarg):
+    return kwarg
+
+dic=foo()
+dic['kwarg']={'name1': 'moses', 'name2': 'HAKIM', 'name3': 'CAESAR'}
+elem=dic['kwarg'].__len__()
+print(str(elem)+ ' elements')
+for i,j in dic['kwarg'].items():
+    print(i,':',j)
+
+lt=boo()
+lt=[1,(1,2),'name1','moses',45]
+elem=lt.__len__()
+len(lt)
+print(str(elem)+ ' elements')
+for i in lt[1]:
+    print(i)
+for i in lt:
+    print(i)
+
+list1=['first name','second name']
+list2=['Moses','Hakim']
+for i,j in enumerate(list1):
+    print(i,'',j)
+
+for i,j in zip(list1,list2):
+    print(i,'',j)
+
+for i in range(len(list1)):
+    print(i,'',list1[i])
+
+list1=[('first name','second name')]
+list2=[('Moses','Hakim')]  
+l=list1+list2
+for i in l:
+    print(i,':',)
+
+l=[44, 33, 12, 3, 1,100]
+even = lambda lt: [i for i in lt if i%2==0]
+print(even(l))
+
+
+
+
+    
+    
